34c5399bf6c70d6663119102820fd5858db08688
Maiharshhu <hj.harsh440@gmail.com>
2025-01-15 08:59:39 +0530

    DB creation

--- ch22/student/models.py
+++ ch22/student/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Profile(models.Model):
     name = models.CharField(max_length=70)
-    email=models.EmailField(max_length=254)
-    city= models.CharField(max_length=70)
+    email = models.EmailField(max_length=254)
+    city = models.CharField(max_length=70)
     roll = models.IntegerField()
-
+    state = models.CharField(max_length=70)
+    comment = models.CharField(max_length=70, default="nothing")
