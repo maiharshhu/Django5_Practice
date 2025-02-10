f991e164fd62737385095a69af7daf6c91af223e
Maiharshhu <hj.harsh440@gmail.com>
2025-02-10 17:07:19 +0530

    custom filter with and without decorator

--- ch37/student/templatetags/customfilter.py
+++ ch37/student/templatetags/customfilter.py
@@ -1,7 +1,13 @@
 from django import template
 register = template.Library()
 
-def myreplace(value,arg):
-    return value.replace(arg, 'We are')
+# without decorator
+# def myreplace(value,arg):
+#     return value.replace(arg, 'We are')
 
-register.filter('iamToweare',myreplace)
+# register.filter('iamToweare',myreplace)
+
+# with decorator
+@register.filter(name='iamToweare')
+def myreplace(value,arg):
+    return value.replace(arg,'We are')
